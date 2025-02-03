82886295893b6e4d59990dbb0509cdc0bd5c4d0d
Tobias Wulvik <tobiaswulvik@Tobiass-MacBook-Pro-2.local>
2025-02-03 08:58:36 +0100

    using ghostty

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,26 @@
 return {
+	-- {
+	-- 	"catppuccin/nvim",
+	-- 	name = "catppuccin",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme("catppuccin")
+	-- 	end,
+	-- },
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
+		-- "slugbyte/lackluster.nvim",
+		-- 	"catppuccin/nvim",
+		"aliqyan-21/darkvoid.nvim",
+		lazy = false,
 		priority = 1000,
+		init = function()
+			vim.cmd.colorscheme("darkvoid")
+		end,
 		config = function()
-			vim.cmd.colorscheme("catppuccin")
+			require("darkvoid").setup({
+				transparent = true,
+				glow = true,
+			})
 		end,
 	},
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return {
 	{
+		"nvim-treesitter/nvim-tree-docs",
+	},
+	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		config = function()
@@ -9,6 +12,19 @@ return {
 				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true },
+				tree_docs = {
+					enabled = true,
+					jsdoc = {
+						slots = {
+							class = { author = true },
+						},
+					},
+				},
+			})
+
+			local wk = require("which-key")
+			wk.add({
+				{ "<leader>cd", "<cmd>TreeDocsGenerate<CR>", desc = "Generate docs" },
 			})
 		end,
 	},
