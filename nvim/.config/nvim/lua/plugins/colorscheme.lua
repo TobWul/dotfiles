a42d788517315e1ad58905d4ac8693db1fb4942f
Tobias Wulvik <tobiaswulvik@Tobiass-MacBook-Pro-2.local>
2025-02-03 15:01:17 +0100

    colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,31 @@
 return {
-	-- {
-	-- 	"catppuccin/nvim",
-	-- 	name = "catppuccin",
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		vim.cmd.colorscheme("catppuccin")
-	-- 	end,
-	-- },
 	{
-		-- "slugbyte/lackluster.nvim",
-		-- 	"catppuccin/nvim",
-		"aliqyan-21/darkvoid.nvim",
-		lazy = false,
+		"catppuccin/nvim",
+		name = "catppuccin",
 		priority = 1000,
-		init = function()
-			vim.cmd.colorscheme("darkvoid")
-		end,
+		lazy = false,
 		config = function()
-			require("darkvoid").setup({
-				transparent = true,
-				glow = true,
+			require("catppuccin").setup({
+				transparent_background = true,
 			})
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
+	-- {
+	-- 	"aliqyan-21/darkvoid.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	init = function()
+	-- 		vim.cmd.colorscheme("darkvoid")
+	-- 	end,
+	-- 	config = function()
+	-- 		require("darkvoid").setup({
+	-- 			transparent = true,
+	-- 			glow = true,
+	-- 			colors = {
+	-- 				visual = "#1D4031",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 }
