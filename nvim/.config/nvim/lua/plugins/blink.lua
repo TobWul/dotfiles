c14e4f617ae82d4248ed7df20f6df9b56ce1b5b0
Tobias Wulvik <tobias.wulvik@gmail.com>
2025-03-13 07:11:15 +0100

    new markdown view

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -51,9 +51,16 @@ return {
 			-- Default list of enabled providers defined so that you can extend it
 			-- elsewhere in your config, without redefining it, due to `opts_extend`
 			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
+				default = { "lsp", "path", "snippets", "buffer", "markdown" },
 				per_filetype = {
 					codecompanion = { "codecompanion" },
+				},
+				providers = {
+					markdown = {
+						name = "RenderMarkdown",
+						module = "render-markdown.integ.blink",
+						fallbacks = { "lsp" },
+					},
 				},
 			},
 		},
